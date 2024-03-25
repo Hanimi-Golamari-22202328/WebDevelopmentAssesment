32d9c2f74c857741457eb92a8bae7bbbd4d93b4c
Brahmateja20007384 <162308626+Brahmateja20007384@users.noreply.github.com>
2024-03-25 21:24:41 +0000

    Add files via upload

---
+++ Doccccc/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const colors = require('colors')
+const moragan = require('morgan')
+const dotenv = require('dotenv')
+const ConnectDB = require('./config/db')
+
+const app = express()
+dotenv.config();
+//middlewares
+ConnectDB();
+app.use(express.json())
+app.use(moragan('dev'))
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+// app.use("api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+//listen port
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`process running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`.bgCyan.white);
+});
